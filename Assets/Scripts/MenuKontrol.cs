3869d66c5bc6849564d5e7571e429e8de252a90c
IsmazilBurak <ismailburakozciftci6@gmail.com>
2024-10-06 14:54:43 +0300

    feat: Finished the game

---
+++ Assets/Scripts/MenuKontrol.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuKontrolu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+        public void PlayMenu()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+        public void PlayGame1()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+        public void PlayGame2()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+
+}
