51e19700dee1fd0cccf90cab07060db5fd4965af
Furkan Yılmaz <furkanyilmaz870@gmail.com>
2024-10-06 14:55:07 +0300

    feat: Add main menu

--- Assets/Scripts/Core/SceneController.cs
+++ Assets/Scripts/Core/SceneController.cs
@@ -7,11 +7,14 @@ using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
+    [Header("Panels")]
     [SerializeField] private RectTransform currentPanel;
     [SerializeField] private List<RectTransform> panels;
+
     [SerializeField] private Vector3 endValue;
     [SerializeField] private float doDurationTime;
     [SerializeField] private AudioClip startSpeech;
+    [SerializeField] private AudioClip emotionPanelSpeech;
     [SerializeField] private GameObject animator;
 
     private void Start()
@@ -23,6 +26,7 @@ public class SceneController : MonoBehaviour
     {
         yield return new WaitForSeconds(5f);
         ChangeScene(panels[1]);
+        currentPanel.DOMoveY(Screen.height / 2, 5f);
         yield return new WaitForSeconds(1f);
         SoundController.Instance.PlaySound(startSpeech);
     }
@@ -32,7 +36,12 @@ public class SceneController : MonoBehaviour
         currentPanel.gameObject.SetActive(false);
         animator.SetActive(false);
         currentPanel = targetPanel;
-        currentPanel.gameObject.SetActive(true);
-        currentPanel.DOMoveY(Screen.height / 2, 5f);
+        currentPanel.gameObject.SetActive(true);    
+    }
+
+    public void OpenEmotionPanel()
+    {
+        ChangeScene(panels[2]);
+        SoundController.Instance.PlaySound(emotionPanelSpeech);
     }
 }
